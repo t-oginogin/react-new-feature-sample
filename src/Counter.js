7ce1c156cc8e2f0e69cbca15aa82e25d64a2ac8c
Tatsuya Ogi <t.oginogin@gmail.com>
2018-12-15 14:23:26 +0900

    setState -> setCount

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react'
 
 const Counter = () => {
-	const [count, setState] = useState(0)
+	const [count, setCount] = useState(0)
 	useEffect(() => { console.log('Changed!')})
 	useEffect(() => { document.title = `Count: ${count}` })
 
 	return (
 		<div>
 			<p>Count: {count}</p>
-			<button onClick={() => setState(0)}>React</button>
-			<button onClick={() => setState(count - 1)}>-</button>
-			<button onClick={() => setState(count + 1)}>+</button>
+			<button onClick={() => setCount(0)}>React</button>
+			<button onClick={() => setCount(count - 1)}>-</button>
+			<button onClick={() => setCount(count + 1)}>+</button>
 		</div>
 	)
 }
