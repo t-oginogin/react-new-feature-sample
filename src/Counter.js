716927c4086a83ff877a30a963f88958e9090ce7
Tatsuya Ogi <t.oginogin@gmail.com>
2018-12-15 13:57:59 +0900

    use "useState"

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-class Counter extends React.Component {
-	state = { count: 0 }
+const Counter = () => {
+	const [count, setState] = useState(0)
 
-	render() {
-		return (
-			<div>
-				<p>Count: {this.state.count}</p>
-				<button onClick={() => this.setState(() => ({count: 0}))}>React</button>
-				<button onClick={() => this.setState(state => ({count: state.count - 1}))}>-</button>
-				<button onClick={() => this.setState(state => ({count: state.count + 1}))}>+</button>
-			</div>
-		)
-	}
+	return (
+		<div>
+			<p>Count: {count}</p>
+			<button onClick={() => setState(0)}>React</button>
+			<button onClick={() => setState(count - 1)}>-</button>
+			<button onClick={() => setState(count + 1)}>+</button>
+		</div>
+	)
 }
 
 export default Counter
