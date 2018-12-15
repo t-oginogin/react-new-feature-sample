c229f5a3e9efd52133ffffc352c0b163640894c6
Tatsuya Ogi <t.oginogin@gmail.com>
2018-12-15 15:54:51 +0900

    use Suspense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
+import AsyncText from './AsyncText'
 
 class App extends Component {
   render() {
-    return <Counter />;
+    return (
+      <>
+        <Counter />
+				<Suspense fallback={<div>Loading...</div>}>
+          <AsyncText text="Hello!" ms={2000} />
+					<AsyncText text="Hello!!!!" ms={3000} />
+        </Suspense>
+      </>
+    );
   }
 }
 
---
+++ src/AsyncText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import TextResource from './TextResource'
+
+const AsyncText = ({text, ms}) => {
+	const resource = TextResource.read([text, ms])
+	return <div>{resource}</div>
+}
+
+export default AsyncText
